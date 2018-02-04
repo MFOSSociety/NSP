0d8db65d80415b31fe229617ca9382d018a0f8cb
Rishi110 <rishieric91@gmail.com>
2018-02-04 15:57:39 +0530

    models constructed and registered

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import usr, book, tool
+
+admin.site.register(usr)
+admin.site.register(book)
+admin.site.register(tool)
