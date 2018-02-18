1bc9a6b8d5005db0714fc085a41e1823e86bcd43
Rishi110 <rishieric91@gmail.com>
2018-02-18 22:53:03 +0530

    State Clear

--- ideate2018/urls.py
+++ ideate2018/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, include
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+
+    path('admin/', admin.site.urls),
 ]
-#superuser name = rishieric
-#password pass = honeyginger
--- ideate2018/wsgi.py
+++ ideate2018/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for ideate2018 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
 
 import os
 
