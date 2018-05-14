e23edcecc4d2bbd732546a326b67dd0d157de69a
Rishi Banerjee <rishieric91@gmail.com>
2018-05-14 19:10:21 +0700

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import  UserChangeForm
 from accounts.models import UserProfile
 
-class RegistrationForm(UserCreationForm):   # extending from superclass
-    email = forms.EmailField(required=True)
 
-    # define meta data
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
@@ -15,12 +14,12 @@ class RegistrationForm(UserCreationForm):   # extending from superclass
             'first_name',
             'last_name',
             'email',
-            'password1',
-            'password2'
+            'password'
+
         )
 
     def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False) # i might have left a loophole here
+        user = super(UserForm, self).save(commit=False)  # i might have left a loophole here
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
@@ -29,6 +28,29 @@ class RegistrationForm(UserCreationForm):   # extending from superclass
             user.save()
 
         return user
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = (
+            'year_of_study',
+            'image',
+            'phone',
+            'stream',
+        )
+
+    def save(self, commit=True):
+        userprofile = super(UserProfileForm, self).save(commit=False)
+        userprofile.year_of_study = self.cleaned_data['year_of_study']
+        userprofile.image = self.cleaned_data['image']
+        userprofile.phone = self.cleaned_data['phone']
+        userprofile.stream = self.cleaned_data['stream']
+
+        if commit:
+            userprofile.save()
+
+        return userprofile
 
 
 class EditProfileForm(UserChangeForm):
@@ -49,9 +71,6 @@ class EditSkillUtilityForm(UserChangeForm):
     class Meta:
         model = UserProfile
         fields = (
-            'skill',
-            'books',
-            'tools',
             'image',
             'year_of_study',
             'stream',
