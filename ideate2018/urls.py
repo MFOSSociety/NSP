014ca32e2ac8194bf29f2aa3bc8874a457de4962
Rishi Banerjee <rishieric91@gmail.com>
2018-05-14 19:10:18 +0700

    models constructed and registered

--- ideate2018/urls.py
+++ ideate2018/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
 #superuser name = rishieric
 #password pass = honeyginger
