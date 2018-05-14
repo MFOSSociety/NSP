d82dbebcbf47b97d6d62d61ec93411caf5601345
Rishi Banerjee <rishieric91@gmail.com>
2018-05-14 19:10:18 +0700

    State Clear

--- transaction/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import usr, book, tool
-
-admin.site.register(usr)
-admin.site.register(book)
-admin.site.register(tool)
--- transaction/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class TransactionConfig(AppConfig):
-    name = 'transaction'
