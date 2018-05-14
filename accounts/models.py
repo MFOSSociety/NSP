cbe0f3f7de7351eb4d4070c127565b93821b92df
Rishi Banerjee <rishieric91@gmail.com>
2018-05-14 19:10:21 +0700

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -3,41 +3,15 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
-class Skill(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Book(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Tool(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # listing for multiple objects
-
-    skill = models.ManyToManyField(Skill, default="", blank=True)
-    books = models.ManyToManyField(Book, default="", blank=True)
-    tools = models.ManyToManyField(Tool, default="", blank=True)
     year_of_study = models.CharField(max_length=3, default="", blank=True)
     stream = models.CharField(max_length=50, default="", blank=True)
     phone = models.IntegerField(default=0, blank=True)
     image = models.ImageField(upload_to='profile_image', blank=True)
 
     def __str__(self):
-        return self.user.first_name + " " + self.user.last_name
+        return self.user.username
 
 
 def create_profile(sender, **kwargs):
