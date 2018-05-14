7971d686926cc15dc4d54027aa8fa4126a893413
Rishi Banerjee <rishieric91@gmail.com>
2018-05-14 19:10:19 +0700

    Synchronized. Updated Code Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,20 @@
 from django.db import models
-# Create your models here.
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    description = models.CharField(max_length=100, default="", blank=True)
-    city = models.CharField(max_length=100, default="", blank=True)
-    website = models.URLField(default="",blank=True)
-    phone = models.IntegerField(default=0,blank=True)
+    skill = models.CharField(max_length=100, default="", blank=True)
+    year_of_study = models.CharField(max_length=3, default="", blank=True)
+    stream = models.CharField(max_length=50, default="", blank=True)
+    books = models.CharField(max_length=100, default="", blank=True)
+    tools = models.CharField(max_length=100, default="", blank=True)
+    phone = models.IntegerField(default=0, blank=True)
     image = models.ImageField(upload_to='profile_image', blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.first_name + " " + self.user.last_name
 
 
 def create_profile(sender, **kwargs):
