30554e9734051e07e6bc52cbfdd206a5854b2658
pratikjain04 <35142725+pratikjain04@users.noreply.github.com>
2018-06-08 13:45:28 +0530

    Added URL to the new forum page

--- ideate2018/urls.py
+++ ideate2018/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from machina.app import board
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
+    path('forum/', include(board.urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
