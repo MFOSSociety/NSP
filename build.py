3e7ffac0208dbc748e842984a5969476bdc652ca
Rishi Banerjee <rishieric91@gmail.com>
2018-06-14 10:04:11 +0700

    Revamped Build Files

--- build.py
+++ build.py
@@ -1,41 +1,51 @@
 import os
-print("\n\n=======================")
-print("Django Configure Script")
-print("=======================")
+import sys
 
-print("\nRunning this file will delete the Database and all the Migrations!")
-print("Are you sure you want to continue[Y/n]?")
+# runserver function
+def runserver():
+	print(colored("=======================", "green"))
+	print(colored("  Running Server", "yellow"))
+	print(colored("=======================", "green"))
+	os.system("python manage.py runserver")
+
+if('termcolor' not in sys.modules):
+	os.system("sudo pip install termcolor")
+
+	
+from termcolor import colored
+print(colored("\n\n=======================", "green"))
+print(colored("Django Configure Script", "yellow"))
+print(colored("=======================", "green"))
+
+print(colored("\nRunning this file will delete the Database and all the Migrations!", "red"))
+print(colored("Are you sure you want to continue[Y/n]?", "magenta"))
 choice = input()
 if(choice == 'n' or choice == 'N'):
-  quit()
+  runserver()
 
-print("=======================")
-print("   Deleting Database")
-print("=======================")
+print(colored("=======================", "green"))
+print(colored("   Deleting Database", "yellow"))
+print(colored("=======================", "green"))
 os.system("rm -rf db.sqlite3")
-print("+++++++Complete++++++++")
+print(colored("+++++++Complete++++++++", "green"))
 
-print("=======================")
-print("  Deleting Migrations")
-print("=======================")
+
+print(colored("=======================", "green"))
+print(colored("  Deleting Migrations", "yellow"))
+print(colored("=======================", "green"))
 os.system("rm -rf accounts/migrations/")
-print("+++++++Complete++++++++")
+print(colored("+++++++Complete++++++++", "green"))
 
-print("=========================")
-print("  Rebuilding Migrations")
-print("=========================")
+print(colored("=========================", "green"))
+print(colored("  Rebuilding Migrations", "yellow"))
+print(colored("=========================", "green"))
 os.system("python manage.py makemigrations accounts")
 os.system("python manage.py migrate")
-print("+++++++Complete++++++++")
+print(colored("+++++++Complete++++++++", "green"))
 
-print("\nDo you want to create a Super User?[Y/n]")
+print(colored("\nDo you want to create a Super User?[Y/n]", "blue"))
 choice = input()
 if(choice == 'n' or choice == 'N'):
-	quit()
+	runserver()
 os.system("python manage.py createsuperuser")
-
-print("\n++++++++++++++++++")
-print("  Running Server")
-print("++++++++++++++++++\n")
-os.system("python manage.py runserver")
 
