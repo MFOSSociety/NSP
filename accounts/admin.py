7610b3863441567bbab36768e7d1ce5930d3cc23
Rishi Banerjee <rishieric91@gmail.com>
2018-06-14 18:06:21 +0700

    Slight Modification to Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import ProjectDetail, User, UserProfile
 
-
+admin.site.site_title  = "NSP"
+admin.site.index_title = "NSP"
+admin.site.site_header = "NSP ADMINISTRATION"
 
 admin.site.register(ProjectDetail)
 admin.site.register(UserProfile)
