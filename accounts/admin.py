9da4cc91930a846748060a968dc48577b34f777d
gjergjk71 <gjergjk71@gmail.com>
2018-06-20 21:47:45 +0200

    Registered Follow model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import ProjectDetail, UserProfile
-from accounts.models import User
+from accounts.models import User,Follow
 
 admin.site.site_title = "NSP"
 admin.site.index_title = "NSP"
@@ -9,3 +9,4 @@ admin.site.site_header = "NSP ADMINISTRATION"
 admin.site.register(User)
 admin.site.register(ProjectDetail)
 admin.site.register(UserProfile)
+admin.site.register(Follow)
