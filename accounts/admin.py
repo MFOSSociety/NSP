6bfbd00999ef3f35ed83cca76aaf5adfaca7b7bc
gjergjk71 <gjergjk71@gmail.com>
2018-06-22 22:27:05 +0200

    Registered ProjectPeopleInterested to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import ProjectDetail, UserProfile
-from accounts.models import User, Follow
+from accounts.models import User, Follow, ProjectPeopleInterested
 
 admin.site.site_title = "NSP"
 admin.site.index_title = "NSP"
@@ -10,3 +10,4 @@ admin.site.register(User)
 admin.site.register(ProjectDetail)
 admin.site.register(UserProfile)
 admin.site.register(Follow)
+admin.site.register(ProjectPeopleInterested)
