161bf3216859b9c3c600b82b9750e95974e2a703
gjergjk71 <gjergjk71@gmail.com>
2018-06-27 18:21:16 +0200

    Removed mptt from INSTALLED_APPS

--- nsp/settings.py
+++ nsp/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mptt',  # to handle the tree of forum instances
     'widget_tweaks',
 
 ]
