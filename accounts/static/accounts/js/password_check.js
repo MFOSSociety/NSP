bf570a87e0b159bcf713f19843d64b2b8be43545
pratikjain04 <35142725+pratikjain04@users.noreply.github.com>
2018-06-30 14:37:46 +0530

    SignUp JS Bug fixed

--- accounts/static/accounts/js/password_check.js
+++ accounts/static/accounts/js/password_check.js
@@ -1,25 +1,24 @@
-function checkPass() {
-    var a = document.getElementById("password1").value;
-    var b = document.getElementById("password2").value;
-    var acheck = /^^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-    if (a === "") {
-        document.getElementById("message").innerHTML = "Please fill password!";
-        return false;
-    }
-    if (acheck.test(a)) {
-        document.getElementById("message").innerHTML = " ";
-    }
-    else if (a === "") {
-        document.getElementById("message").innerHTML = "Please fill password!";
-        return false;
-    }
-    else if (a !== b) {
-        document.getElementById("messages").innerHTML = "Confirm password did not match ";
-        return false;
-    }
-    else {
-        document.getElementById("message").innerHTML = "Password must be at least 8 in lenth and contain  one special character,number,lower case and upper case alphabet";
-        return false;
-    }
-}
 
+function checkPass() {
+var a = document.getElementById("password1").value;
+var b = document.getElementById("password2").value;
+var acheck = /^^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+if (a==="") {
+document.getElementById("message").innerHTML="Please fill password!";
+return false;
+}
+if (acheck.test(a)) {
+document.getElementById("message").innerHTML=" ";
+} else{
+document.getElementById("message").innerHTML="Password must be atleast 8 in lenth and contain one speacial character,number,lower case and upper case alphabet";
+return false;
+}
+if (a==="") {
+document.getElementById("message").innerHTML="Please fill password!";
+return false;
+}
+if (a !== b) {
+document.getElementById("messages").innerHTML="Confirm password did not match ";
+return false;
+}
+}
