bb842a7f0713466443180de603542a3c2e348606
gjergjk71 <gjergjk71@gmail.com>
2018-06-30 13:15:58 +0200

    Added project/<projectID>/<type_>/<ID>/comment path

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('project/<ID>/issues/<status>', views.projectIssues, name='projectIssues'),
     path('project/<ID>/solutions/<status>', views.projectSolutions, name='projectIssues'),
     path('project/<projectID>/<type_>/<ID>', views.viewIssueSolution, name='viewIssueSolution'),
+    path('project/<projectID>/<type_>/<ID>/comment', views.commentIssueSolution, name='commentIssueSolution'),
     path('login/', auth_views.login, {'template_name': 'accounts/login.html'}, name='user_login'),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('registersuccess/', views.SuccesfullRegistrationView, name='registersucess'),
