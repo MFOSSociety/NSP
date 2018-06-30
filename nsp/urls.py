0b5068b8a5c9ca43066a5ad4d60f99b4489098ec
gjergjk71 <gjergjk71@gmail.com>
2018-07-01 00:51:35 +0200

    '/' redirects to '/account/'

--- nsp/urls.py
+++ nsp/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+def redirectToAccount(request):
+	return redirect("/account/")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
+    path('',redirectToAccount,name="redirectToAccount")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
