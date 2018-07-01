7c7a45dc43624ff394f870feb70f976f6addacf8
Rishi Banerjee <rishieric91@gmail.com>
2018-07-01 12:06:20 +0700

    Updated team.html

---
+++ accounts/static/accounts/img/Rishi Banerjee_files/poE1sMEGkzs.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["umUWd"]); }
+
+__d("AYMTHomepagePanelLogger",["BanzaiLogger","Event","tidyEvent"],(function(a,b,c,d,e,f){a={init:function(a,c){b("tidyEvent")(b("Event").listen(a,"click",function(event){b("BanzaiLogger").log("AYMTHomepagePanelLoggerConfig",c)}))}};e.exports=a}),null);
