b59bb889d49d147bf76cbeec3919519177eddc3c
Rishi Banerjee <rishieric91@gmail.com>
2018-07-03 02:46:09 +0700

    Increased size of skill length

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Skill(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
-    skill_name = models.CharField(max_length=20, default="", blank=True)
+    skill_name = models.CharField(max_length=100, default="", blank=True)
 
     def __str__(self):
         return self.skill_name
