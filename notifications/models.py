6f745d4f5c31b15e24184ea45de929ddf15354ba
gjergjk71 <gjergjk71@gmail.com>
2018-07-03 16:10:06 +0200

    Created notification models

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from accounts.models import Issue,Solution,ProjectPeopleInterested,Follow,IssueComment,SolutionComment
 # Create your models here.
+
+class IssueNotification(models.Model):
+	issue = models.ForeignKey(Issue,on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
+
+class SolutionNotification(models.Model):
+	solution = models.ForeignKey(Solution,on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
+
+class InterestedNotification(models.Model):
+	issue = models.ForeignKey(Issue,on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
+
+class FollowNotification(models.Model):
+	follow = models.ForeignKey(Follow,on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
+
+class IssueCommentNotification(models.Model):
+	issueComment = models.ForeignKey(IssueComment,on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
+
+class SolutionCommentNotification(models.Model):
+	solutionComment = models.ForeignKey(SolutionComment,on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
