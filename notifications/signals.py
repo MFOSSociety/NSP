4da99abf8f59a438a5a42bb1cf943bd411a15490
gjergjk71 <gjergjk71@gmail.com>
2018-07-04 21:12:54 +0200

    Created createSolutionNotification signal

--- notifications/signals.py
+++ notifications/signals.py
@@ -2,10 +2,16 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from accounts.models import Issue,Solution,ProjectPeopleInterested,Follow,IssueComment,SolutionComment
-import notifications.models 
+import notifications.models
 
 
 @receiver(post_save, sender=Issue)
 def createIssueNotification(sender, instance, **kwargs):
-	notificationText = "{} created issue #{} to {}".format(instance.user,instance.id,instance.project.project_name)
+	notificationText = "{} created issue #{} on {}".format(instance.user,instance.id,instance.project.project_name)
 	notifications.models.IssueNotification.objects.create(user=instance.project.initiated_by,issue=instance,text=notificationText)
+
+@receiver(post_save, sender=Solution)
+def createSolutionNotification(sender, instance, **kwargs):
+	notificationText = "{} created solution #{} to issue #{} on {}".format(instance.user,instance.id,
+						instance.issue.id,instance.issue.project.project_name)
+	notifications.models.SolutionNotification.objects.create(user=instance.issue.project.initiated_by,solution=instance,text=notificationText)
