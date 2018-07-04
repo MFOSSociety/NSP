fee6e501e7d6a14b08e9c52979d7763268ddae7c
gjergjk71 <gjergjk71@gmail.com>
2018-07-04 20:44:40 +0200

    Removed unnessecary import

--- notifications/models.py
+++ notifications/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 import notifications.signals
 from django.db.models.signals import post_save
-from accounts.models import *
 
 #from accounts.models import Issue,Solution,ProjectPeopleInterested,Follow,IssueComment,SolutionComment
 # Create your models here.
