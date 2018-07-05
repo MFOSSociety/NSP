7589a96d8f770d1fa6afc409c67bafd7e81b1053
gjergjk71 <gjergjk71@gmail.com>
2018-07-05 14:25:06 +0200

    Added __str__ to notification models

--- notifications/models.py
+++ notifications/models.py
@@ -12,33 +12,45 @@ class IssueNotification(models.Model):
 	issue = models.ForeignKey('accounts.Issue',on_delete=models.CASCADE)
 	text = models.CharField(max_length=300)
 	date = models.DateField(auto_now_add=True)
+	def __str__(self):
+		return self.text
 
 class SolutionNotification(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	solution = models.ForeignKey('accounts.Solution',on_delete=models.CASCADE)
 	text = models.CharField(max_length=300)
 	date = models.DateField(auto_now_add=True)
+	def __str__(self):
+		return self.text
 
 class InterestedNotification(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	project = models.ForeignKey('accounts.ProjectDetail',on_delete=models.CASCADE)
 	text = models.CharField(max_length=300)
 	date = models.DateField(auto_now_add=True)
+	def __str__(self):
+		return elf.text
 
 class FollowNotification(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	follow = models.ForeignKey('accounts.Follow',on_delete=models.CASCADE)
 	text = models.CharField(max_length=300)
 	date = models.DateField(auto_now_add=True)
+	def __str__(self):
+		return self.text
 
 class IssueCommentNotification(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	issueComment = models.ForeignKey('accounts.IssueComment',on_delete=models.CASCADE)
 	text = models.CharField(max_length=300)
 	date = models.DateField(auto_now_add=True)
+	def __str__(self):
+		return self.text
 
 class SolutionCommentNotification(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	solutionComment = models.ForeignKey('accounts.SolutionComment',on_delete=models.CASCADE)
 	text = models.CharField(max_length=300)
 	date = models.DateField(auto_now_add=True)
+	def __str__(self):
+		return self.text
