f49409f297c487758d1c7c994254a6ae64da4b13
gjergjk71 <gjergjk71@gmail.com>
2018-07-06 22:34:44 +0200

    Added followNotification_profile to notifc function

--- notifications/views.py
+++ notifications/views.py
@@ -4,20 +4,25 @@ from accounts.models import UserProfile
 # Create your views here.
 
 def getNotifications(request):
-    issueNotifications = IssueNotification.objects.filter(issue__user=request.user).order_by("-id")
+    issueNotifications = IssueNotification.objects.filter(user=request.user).order_by("id")
     issueNotification_profile = {}
     for issueNotifc in issueNotifications:
     	issueNotification_profile[issueNotifc] = UserProfile.objects.get(user=issueNotifc.user)
 
     solutionNotification_profile = {}
-    solutionNotifications = SolutionNotification.objects.filter(solution__user=request.user).order_by("-id")
+    solutionNotifications = SolutionNotification.objects.filter(user=request.user).order_by("id")
     for solutionNotifc in solutionNotifications:
     	solutionNotification_profile[solutionNotifc] = UserProfile.objects.get(user=solutionNotifc.user)
 
-    followNotifications = FollowNotification.objects.filter(follow__following=request.user).order_by("-id")
-    issueCommentNotifications = IssueCommentNotification.objects.filter(issueComment__user=request.user).order_by("-id")
-    solutionCommentNotification = SolutionCommentNotification.objects.filter(solutionComment__user=request.user).order_by("-id")
-    result_list = {"issuesNotifc":issueNotification_profile,"solutionsNotifc":solutionNotification_profile,
-                    "followNotifcs":followNotifications,"issueCommentNotifc":issueCommentNotifications,
+    followNotification_profile = {}
+    followNotifications = FollowNotification.objects.filter(user=request.user).order_by("id")
+    for followNotifc in followNotifications:
+    	followNotification_profile[followNotifc] = UserProfile.objects.get(user=followNotifc.user)
+
+    followNotifications = FollowNotification.objects.filter(follow__following=request.user).order_by("id")
+    issueCommentNotifications = IssueCommentNotification.objects.filter(issueComment__user=request.user).order_by("id")
+    solutionCommentNotification = SolutionCommentNotification.objects.filter(solutionComment__user=request.user).order_by("id")
+    notifications = {"issuesNotifc":issueNotification_profile,"solutionsNotifc":solutionNotification_profile,
+                    "followNotifc":followNotification_profile,"issueCommentNotifc":issueCommentNotifications,
                     "solutionCommentNotifc":solutionCommentNotification}
-    return result_list
+    return notifications
