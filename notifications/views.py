a3290b318ae57f33b452fa21ae0a7cd91cc88dbd
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 01:34:30 +0200

    Used OrderedDict() to notifc.._profile var

--- notifications/views.py
+++ notifications/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from notifications.models import Notification
 from accounts.models import UserProfile
+from collections import OrderedDict
 # Create your views here.
 
 def getNotifications(request):
-    notification_profile = {}
-    notifications = Notification.objects.filter(user=request.user)
+    notification_profile = OrderedDict()
+    notifications = Notification.objects.filter(user=request.user).order_by("-id")
     for notification in  notifications:
         notification_profile[notification] = UserProfile.objects.get(user=notification.from_user)
     return notification_profile
