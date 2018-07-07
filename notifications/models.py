c84c504b1b0386dc914386689dda3aadd0c10feb
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 00:24:05 +0200

    Changed status default value

--- notifications/models.py
+++ notifications/models.py
@@ -12,7 +12,7 @@ class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
     redirect = models.CharField(max_length=500)
-    status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")),default="1")
+    status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")),default="0")
 
     date = models.DateField(auto_now_add=True)
     time = models.DateField(auto_now_add=True)
