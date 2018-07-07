98e34acd3e58bf60c1b620aa6865317d7ca56dac
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 00:12:31 +0200

    Removed getNotifications

--- notifications/views.py
+++ notifications/views.py
@@ -4,25 +4,4 @@ from accounts.models import UserProfile
 # Create your views here.
 
 def getNotifications(request):
-    issueNotifications = IssueNotification.objects.filter(user=request.user).order_by("id")
-    issueNotification_profile = {}
-    for issueNotifc in issueNotifications:
-    	issueNotification_profile[issueNotifc] = UserProfile.objects.get(user=issueNotifc.issue.user)
-
-    solutionNotification_profile = {}
-    solutionNotifications = SolutionNotification.objects.filter(user=request.user).order_by("id")
-    for solutionNotifc in solutionNotifications:
-    	solutionNotification_profile[solutionNotifc] = UserProfile.objects.get(user=solutionNotifc.solution.user)
-
-    followNotification_profile = {}
-    followNotifications = FollowNotification.objects.filter(user=request.user).order_by("id")
-    for followNotifc in followNotifications:
-    	followNotification_profile[followNotifc] = UserProfile.objects.get(user=followNotifc.follow.follower)
-
-    followNotifications = FollowNotification.objects.filter(follow__following=request.user).order_by("id")
-    issueCommentNotifications = IssueCommentNotification.objects.filter(issueComment__user=request.user).order_by("id")
-    solutionCommentNotification = SolutionCommentNotification.objects.filter(solutionComment__user=request.user).order_by("id")
-    notifications = {"issuesNotifc":issueNotification_profile,"solutionsNotifc":solutionNotification_profile,
-                    "followNotifc":followNotification_profile,"issueCommentNotifc":issueCommentNotifications,
-                    "solutionCommentNotifc":solutionCommentNotification}
-    return notifications
+    pass
