07a2716fc8dab5b00b844eb9efb8c87eb78b9112
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 00:15:24 +0200

    Created createIssueNotification signal

--- notifications/signals.py
+++ notifications/signals.py
@@ -4,3 +4,8 @@ from django.dispatch import receiver
 from accounts.models import Issue,Solution,ProjectPeopleInterested,Follow,IssueComment,SolutionComment
 import notifications.models
 
+@receiver(post_save, sender=Issue)
+def createIssueNotification(sender, instance, **kwargs):
+	notificationText = "{} created issue #{} on {}".format(instance.user,instance.id,instance.project.project_name)
+	redirect = "project/{}/issue/{}".format(instance.project.id,instance.id)
+	notifications.models.Notification.objects.create(user=instance.project.initiated_by,redirect=redirect,text=notificationText)
