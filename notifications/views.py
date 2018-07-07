29a4c86e7d61e248f289536be6efba73b560b89a
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 00:52:35 +0200

    Created getNotifications view

--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from notifications.models import *
+from notifications.models import Notification
 from accounts.models import UserProfile
 # Create your views here.
 
 def getNotifications(request):
-    pass
+    notification_profile = {}
+    notifications = Notification.objects.all(request.user)
+    for notification in  notifications:
+        notification_profile[notification] = UserProfile.objects.get(user=notification.from_user)
+    return notification_profile
