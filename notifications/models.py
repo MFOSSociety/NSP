1f0be1979c35473dcbd1f4f0318f89b429261b72
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 00:04:18 +0200

    Deleted all models, created Notification model

--- notifications/models.py
+++ notifications/models.py
@@ -8,67 +8,13 @@ from accounts.models import *
 # from accounts.models import Issue,Solution,ProjectPeopleInterested,Follow,IssueComment,SolutionComment
 # Create your models here.
 
-class IssueNotification(models.Model):
+class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    issue = models.ForeignKey('accounts.Issue', on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
-    date = models.DateField(auto_now_add=True)
+    redirect = models.CharField(max_length=500)
     status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")))
-
-    def __str__(self):
-        return self.text
-
-
-class SolutionNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    solution = models.ForeignKey('accounts.Solution', on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
+    
     date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")))
-
-    def __str__(self):
-        return self.text
-
-
-class InterestedNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    project = models.ForeignKey('accounts.ProjectDetail', on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")))
-
-    def __str__(self):
-        return elf.text
-
-
-class FollowNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    follow = models.ForeignKey('accounts.Follow', on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")))
-
-    def __str__(self):
-        return self.text
-
-
-class IssueCommentNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    issueComment = models.ForeignKey('accounts.IssueComment', on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")))
-
-    def __str__(self):
-        return self.text
-
-
-class SolutionCommentNotification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    solutionComment = models.ForeignKey('accounts.SolutionComment', on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=(("1", "Seen"), ("0", "Unseen")))
-
+    time = models.DateField(auto_now_add=True)
     def __str__(self):
         return self.text
