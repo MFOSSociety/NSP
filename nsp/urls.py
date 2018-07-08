12638c20635a5e4f8de6d057ac7fff9c81efbbcc
Rishi Banerjee <rishieric91@gmail.com>
2018-07-08 20:16:24 +0700

    userProfile API made to work

--- nsp/urls.py
+++ nsp/urls.py
@@ -3,6 +3,9 @@ from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from nspapi.resources import UserProfileResource
+
+user_profile_resource = UserProfileResource()
 
 
 def redirectToAccount(request):
@@ -13,6 +16,7 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('account/', include('accounts.urls')),
                   path('notifications/', include('notifications.urls')),
+                  path('api/', include(user_profile_resource.urls)),
                   path('', redirectToAccount, name="redirectToAccount")
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
