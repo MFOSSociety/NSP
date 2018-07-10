6e8912a4c5da56430fb5f1dc9fe0c9f8cb3cc522
gjergjk71 <gjergjk71@gmail.com>
2018-07-10 19:02:34 +0200

    Created chatFriend view

--- nspmessage/views.py
+++ nspmessage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Message
 # Create your views here.
+
+def chatFriend(request,username):
+	if request.is_ajax():
+		Message.objects.create(
+			sender=request.user,
+			receiver=request.user,
+			msg_content=ajaxSender)
+	return render(request,"chat.html")
+	
