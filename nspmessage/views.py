71763263cd75e9dbb9f2a46ab76d9b0a2154528c
gjergjk71 <gjergjk71@gmail.com>
2018-07-10 21:10:24 +0200

    Created newMessage view

--- nspmessage/views.py
+++ nspmessage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Message
 from django.contrib.auth.models import User
 from nspmessage.models import Message
@@ -12,7 +12,16 @@ def chatFriend(request,username):
 	receiver_user_profile = UserProfile.objects.get(user=receiver)
 	participants = [sender,receiver]
 	messages = Message.objects.filter(sender__in=participants,receiver__in=participants)
-	context = {"messages":messages,"sender_user_profile":current_user_profile,
+	context = {"messages":messages,"receiver":receiver,"sender_user_profile":current_user_profile,
 						"receiver_user_profile":receiver_user_profile}
 	return render(request,"chat.html",context)
-	
+
+def newMessage(request,username):
+	sender = request.user
+	receiver = User.objects.get(username=username)
+	message = request.POST.get("sender_message")
+	Message.objects.create(
+					sender=sender,	   
+					receiver=receiver,
+					msg_content=message)
+	return redirect("/chat/{}".format(username))
