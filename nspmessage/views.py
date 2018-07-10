8d5a591ae8bcd2319289c5ee03c641a6df0f84fb
gjergjk71 <gjergjk71@gmail.com>
2018-07-10 20:54:11 +0200

    chatFriend view passes messages and profiles to chatFriend.html

--- nspmessage/views.py
+++ nspmessage/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Message
+from django.contrib.auth.models import User
+from nspmessage.models import Message
+from accounts.models import UserProfile
 # Create your views here.
 
 def chatFriend(request,username):
-	if request.is_ajax():
-		Message.objects.create(
-			sender=request.user,
-			receiver=request.user,
-			msg_content=ajaxSender)
-	return render(request,"chat.html")
+	sender = request.user
+	receiver = User.objects.get(username=username)
+	current_user_profile = UserProfile.objects.get(user=request.user)
+	receiver_user_profile = UserProfile.objects.get(user=receiver)
+	participants = [sender,receiver]
+	messages = Message.objects.filter(sender__in=participants,receiver__in=participants)
+	context = {"messages":messages,"sender_user_profile":current_user_profile,
+						"receiver_user_profile":receiver_user_profile}
+	return render(request,"chat.html",context)
 	
