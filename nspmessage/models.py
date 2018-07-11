25440b748a0562aa28b0acc1671e367269ba365a
Rishi Banerjee <rishieric91@gmail.com>
2018-07-11 20:53:58 +0700

    Refactor and Reformat in nspmessage

--- nspmessage/models.py
+++ nspmessage/models.py
@@ -3,12 +3,7 @@ from accounts.models import User
 
 
 class Message(models.Model):
-<<<<<<< HEAD
     sender = models.ForeignKey(User, related_name="sender", on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name="receiver", on_delete=models.CASCADE)
-=======
-    sender = models.ForeignKey(User, related_name="sender",on_delete=models.CASCADE)
-    receiver = models.ForeignKey(User, related_name="receiver",on_delete=models.CASCADE)
->>>>>>> 1cd7de5d7287cd51ec41b873bfaedbf7a183aa32
     msg_content = models.CharField(max_length=140, blank=False, null=False)
     created_at = models.DateField(auto_now_add=True)
--- nspmessage/views.py
+++ nspmessage/views.py
@@ -1,72 +1,76 @@
-from django.shortcuts import render,redirect
-from .models import Message
-from django.contrib.auth.models import User
-from nspmessage.models import Message
-from accounts.models import UserProfile,Follow
 from collections import OrderedDict
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from accounts.models import UserProfile, Follow
+from nspmessage.models import Message
+
 
 def getConversations(request):
-	sender = request.user
-	participants = [sender]
-	followings = Follow.objects.filter(follower=sender)
-	for following in followings:
-		participants.append(following.following)
-	conversations = OrderedDict()
-	conv_messages = Message.objects.filter(sender__in=participants,receiver__in=participants).order_by("id")
-	for conv_message in conv_messages:
-		conv_participants = [conv_message.sender,conv_message.receiver]
-		last_message = Message.objects.filter(sender__in=conv_participants,receiver__in=conv_participants).order_by("-id")[0].msg_content
-		if conv_participants[0] == request.user:
-			try:
-				if not conversations[conv_participants[1]]:
-					contact_profile = UserProfile.objects.get(user=conv_participants[1])
-					conversations[conv_participants[1]] = {"profile":contact_profile,"last_message":last_message}
-			except KeyError:
-				contact_profile = UserProfile.objects.get(user=conv_participants[1])
-				conversations[conv_participants[1]] = {"profile":contact_profile,"last_message":last_message}			
-		else:
-			try:
-				if not conversations[conv_participants[0]]:
-					contact_profile = UserProfile.objects.get(user=conv_participants[0])
-					conversations[conv_participants[0]] = {"profile":contact_profile,"last_message":last_message}
-			except KeyError:
-				contact_profile = UserProfile.objects.get(user=conv_participants[0])
-				conversations[conv_participants[0]] = {"profile":contact_profile,"last_message":last_message}	
-	for participant in participants:
-		if participant != request.user:
-			contact_profile = UserProfile.objects.get(user=participant)
-			try:
-				if not conversations[participant]:
-					conversations[participant] = {"profile":contact_profile,"last_message":"Nothing to show"}
-			except KeyError:
-				conversations[participant] = {"profile":contact_profile,"last_message":"Nothing to show"}
-	return conversations
+    sender = request.user
+    participants = [sender]
+    followings = Follow.objects.filter(follower=sender)
+    for following in followings:
+        participants.append(following.following)
+    conversations = OrderedDict()
+    conv_messages = Message.objects.filter(sender__in=participants, receiver__in=participants).order_by("id")
+    for conv_message in conv_messages:
+        conv_participants = [conv_message.sender, conv_message.receiver]
+        last_message = \
+        Message.objects.filter(sender__in=conv_participants, receiver__in=conv_participants).order_by("-id")[
+            0].msg_content
+        if conv_participants[0] == request.user:
+            try:
+                if not conversations[conv_participants[1]]:
+                    contact_profile = UserProfile.objects.get(user=conv_participants[1])
+                    conversations[conv_participants[1]] = {"profile": contact_profile, "last_message": last_message}
+            except KeyError:
+                contact_profile = UserProfile.objects.get(user=conv_participants[1])
+                conversations[conv_participants[1]] = {"profile": contact_profile, "last_message": last_message}
+        else:
+            try:
+                if not conversations[conv_participants[0]]:
+                    contact_profile = UserProfile.objects.get(user=conv_participants[0])
+                    conversations[conv_participants[0]] = {"profile": contact_profile, "last_message": last_message}
+            except KeyError:
+                contact_profile = UserProfile.objects.get(user=conv_participants[0])
+                conversations[conv_participants[0]] = {"profile": contact_profile, "last_message": last_message}
+    for participant in participants:
+        if participant != request.user:
+            contact_profile = UserProfile.objects.get(user=participant)
+            try:
+                if not conversations[participant]:
+                    conversations[participant] = {"profile": contact_profile, "last_message": "Nothing to show"}
+            except KeyError:
+                conversations[participant] = {"profile": contact_profile, "last_message": "Nothing to show"}
+    return conversations
+
 
 def chat(request):
-	conversations = getConversations(request)
-	context = {"conversations":conversations}
-	return render(request,"chat.html",context)
+    conversations = getConversations(request)
+    context = {"conversations": conversations}
+    return render(request, "chatFriend.html", context)
 
-def chatFriend(request,username):
-	sender = request.user
-	receiver = User.objects.get(username=username)
-	current_user_profile = UserProfile.objects.get(user=request.user)
-	receiver_user_profile = UserProfile.objects.get(user=receiver)
-	current_participants = [sender,receiver]
-	messages = Message.objects.filter(sender__in=current_participants,receiver__in=current_participants).order_by("id")
-	conversations = getConversations(request)
 
-	context = {"messages":messages,"receiver":receiver,"sender_user_profile":current_user_profile,
-						"receiver_user_profile":receiver_user_profile,"conversations":conversations}
-	return render(request,"chatFriend.html",context)
+def chatFriend(request, username):
+    sender = request.user
+    receiver = User.objects.get(username=username)
+    current_user_profile = UserProfile.objects.get(user=request.user)
+    receiver_user_profile = UserProfile.objects.get(user=receiver)
+    current_participants = [sender, receiver]
+    messages = Message.objects.filter(sender__in=current_participants, receiver__in=current_participants).order_by("id")
+    conversations = getConversations(request)
 
-def newMessage(request,username):
-	sender = request.user
-	receiver = User.objects.get(username=username)
-	message = request.POST.get("sender_message")
-	Message.objects.create(
-					sender=sender,	   
-					receiver=receiver,
-					msg_content=message)
-	return redirect("/chat/{}".format(username))
+    context = {"messages": messages, "receiver": receiver, "sender_user_profile": current_user_profile,
+               "receiver_user_profile": receiver_user_profile, "conversations": conversations}
+    return render(request, "chatFriend.html", context)
+
+
+def newMessage(request, username):
+    sender = request.user
+    receiver = User.objects.get(username=username)
+    message = request.POST.get("sender_message")
+    Message.objects.create(
+        sender=sender,
+        receiver=receiver,
+        msg_content=message)
+    return redirect("/chat/{}".format(username))
