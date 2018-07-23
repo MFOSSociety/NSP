2b5c920d4c391e248e952cd10a37b164c037cbff
gjergjk71 <gjergjk71@gmail.com>
2018-07-23 16:50:59 +0200

    Moved project related paths to project app

--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 urlpatterns = [
+	path('project/delete/<ID>', views.deleteProject, name="deleteProject"),
+    path('project/start/', views.ProjectDescribeView, name='start_project'),
+    path('project/active/', views.ProjectsListView, name='project_list_view'),
+    path("project/interested/<ID>", views.interestedList, name="interestedList"),
+    path("project/addInterested/<ID>", views.addInterested, name="addInterested"),
+    path("project/active/removeInsterested/<ID>", views.removeInsterested, name="removeInsterested"),
+    path('project/<ID>/edit', views.projectEdit, name='projectEdit'),
+    path('project/<project_id>/', views.ProjectDetailView, name='view_project_detail'),
 ]
