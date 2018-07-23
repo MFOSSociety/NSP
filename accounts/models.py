678046f16b850dad37203d4a7d909e257d5e32ae
gjergjk71 <gjergjk71@gmail.com>
2018-07-23 16:45:51 +0200

    Moved project models to project app

--- accounts/models.py
+++ accounts/models.py
@@ -12,28 +12,6 @@ class Skill(models.Model):
     def __str__(self):
         return self.skill_name
 
-
-class ProjectDetail(models.Model):
-    project_name = models.CharField(max_length=50, default="", blank=False)
-    initiated_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    mentor_name = models.CharField(max_length=50, default="", blank=False)
-    branch = models.CharField(max_length=50, blank=True)
-    description = models.TextField()
-    paid = models.BooleanField(default=False)
-    start_date = models.DateField(default=datetime.datetime.now)
-
-    def __str__(self):
-        return self.project_name + " under " + self.mentor_name
-
-
-class ProjectPeopleInterested(models.Model):
-    user = models.ForeignKey(User, related_name="user", on_delete=models.CASCADE, )
-    project = models.ForeignKey(ProjectDetail, related_name="project", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "{} is interested in {}".format(self.user.username, self.project)
-
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
     ratings = models.IntegerField(null=True, default=0, blank=True)
