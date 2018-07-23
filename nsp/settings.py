23fbb23b3eb055b21b8a79b7c870b6efc7016db8
gjergjk71 <gjergjk71@gmail.com>
2018-07-23 17:04:55 +0200

    Included project.issueSolution to INSTALLED_APPS

--- nsp/settings.py
+++ nsp/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'nspapi',
     'nspmessage',
     'project',
+    'project.issueSolution',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
