25ab240623f3588d39bf14c9a378f87b648dc52a
gjergjk71 <gjergjk71@gmail.com>
2018-07-23 16:52:35 +0200

    Removed project/ from all paths

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+
 urlpatterns = [
-	path('project/delete/<ID>', views.deleteProject, name="deleteProject"),
-    path('project/start/', views.ProjectDescribeView, name='start_project'),
-    path('project/active/', views.ProjectsListView, name='project_list_view'),
-    path("project/interested/<ID>", views.interestedList, name="interestedList"),
-    path("project/addInterested/<ID>", views.addInterested, name="addInterested"),
-    path("project/active/removeInsterested/<ID>", views.removeInsterested, name="removeInsterested"),
-    path('project/<ID>/edit', views.projectEdit, name='projectEdit'),
-    path('project/<project_id>/', views.ProjectDetailView, name='view_project_detail'),
+	path('delete/<ID>', views.deleteProject, name="deleteProject"),
+    path('start/', views.ProjectDescribeView, name='start_project'),
+    path('active/', views.ProjectsListView, name='project_list_view'),
+    path("interested/<ID>", views.interestedList, name="interestedList"),
+    path("addInterested/<ID>", views.addInterested, name="addInterested"),
+    path("active/removeInsterested/<ID>", views.removeInsterested, name="removeInsterested"),
+    path('<ID>/edit', views.projectEdit, name='projectEdit'),
+    path('<project_id>/', views.ProjectDetailView, name='view_project_detail'),
 ]
