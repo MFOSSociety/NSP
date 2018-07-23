f1f4b74a2dae61c22cdf56ed7d099757a7f990ff
gjergjk71 <gjergjk71@gmail.com>
2018-07-23 17:03:53 +0200

    Moved issue/solution related models to issueSolution app

--- project/issueSolution/models.py
+++ project/issueSolution/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class Issue(models.Model):
+    project = models.ForeignKey(ProjectDetail, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=(("1", "Open"), ("0", "Closed")))
+
+    def __str__(self):
+        return "#{} - {}".format(self.id, self.title)
+
+
+class Solution(models.Model):
+    user = models.ForeignKey(User, related_name="user_solution", on_delete=models.CASCADE)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=(("0", "Open"), ("1", "Accepted"), ("2", "Not Accepted")))
+
+    def __str__(self):
+        return "#{} - {}".format(self.id, self.title)
+
+
+class IssueComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+
+class SolutionComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    solution = models.ForeignKey(Solution, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date = models.DateField(auto_now_add=True)
