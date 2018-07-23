856ee725f3014789576863a3ee99033712c36a67
gjergjk71 <gjergjk71@gmail.com>
2018-07-23 17:14:59 +0200

    Imported nessesecary models

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from accounts.models import Issue, Solution, ProjectPeopleInterested, Follow, IssueComment, SolutionComment
+from project.models import ProjectPeopleInterested
+from project.issueSolution.models import Issue, Solution, IssueComment, SolutionComment
+from accounts.models import Follow
 import notifications.models
 
 
