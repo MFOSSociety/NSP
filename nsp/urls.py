bc58790bb91b5779926529e10daa03e17812e37a
Rishi Banerjee <rishieric91@gmail.com>
2018-07-25 01:09:29 +0700

    Removed Redudant API URL's

--- nsp/urls.py
+++ nsp/urls.py
@@ -19,7 +19,7 @@ project_detail_resource = ProjectDetailResource()
 project_people_interested_resource = ProjectPeopleInterestedResource()
 issue_resource = IssueResource()
 issue_comment_resource = IssueCommentResource()
-follow_resource  = FollowResource()
+follow_resource = FollowResource()
 
 
 def redirectToAccount(request):
@@ -30,14 +30,7 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('account/', include('accounts.urls')),
                   path('notifications/', include('notifications.urls')),
-                  path("project/",include("project.urls")),
-                  path('api/users_api/', include(user_profile_resource.urls)),
-                  path('api/skills_api/', include(skill_resource.urls)),
-                  path('api/project_detail_api/', include(project_detail_resource.urls)),
-                  path('api/project_people_interested_api/', include(project_people_interested_resource.urls)),
-                  path('api/issue_api/', include(issue_resource.urls)),
-                  path('api/issue_comment_api/', include(issue_comment_resource.urls)),
-                  path('api/follow_api/', include(follow_resource.urls)),
+                  path("project/", include("project.urls")),
                   path('', redirectToAccount, name="redirectToAccount"),
                   path('api-auth/', include('rest_framework.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
