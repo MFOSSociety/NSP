510e6e1c52e2b8e0ded82a76de7fe885bc1e5575
Rishi Banerjee <rishieric91@gmail.com>
2018-07-25 01:50:19 +0700

    Created ProjectDetail Serializer

--- rest/serializers.py
+++ rest/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from accounts.models import UserProfile
+from project.models import ProjectDetail
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -19,3 +20,9 @@ class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserProfile
         fields = ('user', 'ratings', 'year', 'branch', 'stream', 'gender', 'position', 'bio')
+
+
+class ProjectDetailSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ProjectDetail
+        fields = ('project_name', 'initiated_by', 'mentor_name', 'branch', 'description', 'paid', 'start_date')
