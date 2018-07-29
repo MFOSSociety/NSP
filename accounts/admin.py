d260f983cf39236838634314e340836e1e7a9376
Rishi Banerjee <rishieric91@gmail.com>
2018-07-30 06:40:51 +0700

    Corrected imports in accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from accounts.models import *
+from project.models import ProjectDetail
+from accounts.models import UserProfile
 
 admin.site.site_title = "NSP"
 admin.site.index_title = "NSP"
