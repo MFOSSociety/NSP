08d2958152b09e5bad6da86f55a89223572adf41
Rishi Banerjee <rishieric91@gmail.com>
2018-07-30 07:03:19 +0700

    Fixed project/admin.py

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ProjectDetail,ProjectPeopleInterested
+
+from .models import ProjectDetail, ProjectPeopleInterested
 
 # Register your models here.
 
