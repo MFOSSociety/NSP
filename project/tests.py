aa0e5565bd4ae8ab6804dcff2e89c3cd23f38877
gjergjk71 <gjergjk71@gmail.com>
2018-07-31 01:40:56 +0200

    Removed TestLoginRequired

--- project/tests.py
+++ project/tests.py
@@ -8,18 +8,6 @@ from django.contrib.auth import authenticate
 testDebug = True
 # Create your tests here.
 
-class TestLoginRequired(TestCase):
-	def setUp(self):
-		self.pathnames_args = {}
-	def test_call_view_denies_anonymous(self):
-		if testDebug:
-			print("test_call_view_denies_anonymous")
-		for pathname,args in self.pathnames_args.items():
-			response = self.client.get(reverse(pathname,args=args), follow=True)
-			result = self.assertRedirects(response,reverse("user_login") + "?next=" + reverse(pathname,args=args))
-			if testDebug:
-				print("Tested {} - {}".format(pathname,result))
-
 class TestProjectViewsLoginRequired(TestLoginRequired):
 	def setUp(self):
 		self.user_object = User.objects.create(username="testing",
