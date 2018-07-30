0b90139f8d39b5ac9e54000320bd1652aebcbeb9
gjergjk71 <gjergjk71@gmail.com>
2018-07-31 00:31:34 +0200

    Created TestIssueSolutionViewsLoginRequired

--- project/issueSolution/tests.py
+++ project/issueSolution/tests.py
@@ -1,12 +1,14 @@
-"""from django.test import TestCase
+from django.test import TestCase
 from django.shortcuts import reverse
 from project.models import ProjectDetail
+from project.tests import TestLoginRequired
+from project.issueSolution.models import Issue,IssueComment,Solution,SolutionComment
 from django.contrib.auth.models import User
 
 testDebug = True
 # Create your tests here.
 
-class TestProjectViewsLoginRequired(TestCase):
+class TestProjectViewsLoginRequired(TestLoginRequired):
 	def setUp(self):
 		self.user_object = User.objects.create(username="testing",
 												password="testing")
@@ -19,19 +21,30 @@ class TestProjectViewsLoginRequired(TestCase):
 															description="testing")
 		if testDebug:
 			print("project_object created")
-		self.interested_object = ProjectPeopleInterested.objects.create(user=self.user_object,
-																		project=self.project_object)
+		self.issue_object = Issue.objects.create(user=self.user_object,
+												project=self.project_object,
+												title="testing",
+												description="testing")
 		if testDebug:
-			print("interested_object created")
-		self.pathnames_args = {"start_project":[],"project_list_view":[],
-			"interested_list":[self.project_object.id],"view_project_detail":[self.project_object.id],
-			"add_interested":[self.project_object.id],"remove_interested":[self.interested_object.id],
-			"project_edit":[self.project_object.id],"delete_project":[self.project_object.id]}
-	def test_call_view_denies_anonymous(self):
+			print("issue_object created")
+		self.solution_object = Solution.objects.create(issue=self.issue_object,
+														user=self.user_object,
+														title="testing",
+														description="testing")
 		if testDebug:
-			print("test_call_view_denies_anonymous")
-		for pathname,args in self.pathnames_args.items():
-			response = self.client.get(reverse(pathname,args=args), follow=True)
-			result = self.assertRedirects(response,reverse("user_login") + "?next=" + reverse(pathname,args=args))
-			if testDebug:
-				print("Tested {} - {}".format(pathname,result))"""
+			print("solution_object created")
+		self.issueComment_object = IssueComment.objects.create(issue=self.issue_object,
+																user=self.user_object,
+																comment="testing")
+
+		if testDebug:
+			print("issueComment_object created")
+		self.solutionComment_object = SolutionComment.objects.create(solution=self.solution_object,
+														user=self.user_object,
+														comment="testing")
+		if testDebug:
+			print("solutionComment_object created")
+		self.pathnames_args = {"deleteIssueSolution":["issue",self.solution_object.id],"projectIssues":[self.project_object.id,"open"],
+			"projectSolutions":[self.project_object.id,"open"],"commentIssueSolution":[self.project_object.id,"issue",self.issue_object.id],
+			"editIssueSolution":[self.project_object.id,"issue",self.issue_object.id],"createIssueSolution":[self.project_object.id,"issue"],
+			"viewIssueSolution":[self.project_object.id,"issue",self.issue_object.id],"changeStatusIssueSolution":[self.project_object.id,"issue",self.issue_object.id,"open"]}
