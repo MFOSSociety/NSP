dce60d4872d94fd6ade6d5ec7814fd3a8e2710eb
Rishi Banerjee <rishieric91@gmail.com>
2018-07-30 07:04:33 +0700

    Fixed project/urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path,include
+from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
-	path('delete/<ID>', views.deleteProject, name="deleteProject"),
+    path('delete/<ID>', views.deleteProject, name="deleteProject"),
     path('start/', views.ProjectDescribeView, name='start_project'),
     path('active/', views.ProjectsListView, name='project_list_view'),
     path("interested/<ID>", views.interestedList, name="interestedList"),
@@ -10,5 +11,5 @@ urlpatterns = [
     path("active/removeInsterested/<ID>", views.removeInsterested, name="removeInsterested"),
     path('<ID>/edit', views.projectEdit, name='projectEdit'),
     path('<project_id>/', views.ProjectDetailView, name='view_project_detail'),
-    path("",include("project.issueSolution.urls")),
+    path("", include("project.issueSolution.urls")),
 ]
