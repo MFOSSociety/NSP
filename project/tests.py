2ddf4fa630d64022a23536e74bf5887b23889257
gjergjk71 <gjergjk71@gmail.com>
2018-07-30 23:41:32 +0200

    Minor changes to TestProjectViewsLoginRequired

--- project/tests.py
+++ project/tests.py
@@ -2,11 +2,13 @@ from django.test import TestCase
 from django.shortcuts import reverse
 from project.models import ProjectDetail,ProjectPeopleInterested
 from django.contrib.auth.models import User
+from django.test import Client
+from django.contrib.auth import authenticate
 
 testDebug = True
 # Create your tests here.
 
-class TestProjectLoginRequired(TestCase):
+class TestProjectViewsLoginRequired(TestCase):
 	def setUp(self):
 		self.user_object = User.objects.create(username="testing",
 												password="testing")
@@ -25,7 +27,7 @@ class TestProjectLoginRequired(TestCase):
 			print("interested_object created")
 		self.pathnames_args = {"start_project":[],"project_list_view":[],
 			"interested_list":[self.project_object.id],"view_project_detail":[self.project_object.id],
-			"add_interested":[self.project_object.id],"remove_interested":["1"],
+			"add_interested":[self.project_object.id],"remove_interested":[self.interested_object.id],
 			"project_edit":[self.project_object.id],"delete_project":[self.project_object.id]}
 	def test_call_view_denies_anonymous(self):
 		if testDebug:
@@ -35,3 +37,25 @@ class TestProjectLoginRequired(TestCase):
 			result = self.assertRedirects(response,reverse("user_login") + "?next=" + reverse(pathname,args=args))
 			if testDebug:
 				print("Tested {} - {}".format(pathname,result))
+
+"""class Test_delete_project_view(TestCase):
+	def setUp(self):
+		self.c = Client()
+		self.user_object = User.objects.create(username="testing",
+												password="testing")
+		if testDebug:
+			print("user_object created")
+		self.project_object = ProjectDetail.objects.create(project_name="testing",
+															initiated_by=self.user_object,
+															mentor_name="testing",
+															branch="testing",
+															description="testing")
+		if testDebug:
+			print("project_object created")
+			print(self.project_object)	
+	def test_get_method(self):
+		self.login = authenticate(username=self.user_object.username,password=self.user_object.password) 
+		self.assertTrue(self.login)
+		response = self.c.get(reverse("delete_project",args=[self.project_object.id]))
+		print(response)
+		self.assertEqual(response.status_code, 200)"""
