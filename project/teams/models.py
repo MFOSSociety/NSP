65ea594588263eafdaa295ea909b6b67902b3d48
gjergjk71 <gjergjk71@gmail.com>
2018-08-02 23:48:31 +0200

    Created model Team

--- project/teams/models.py
+++ project/teams/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from project.models import ProjectDetail
 # Create your models here.
+
+class Team(models.Model):
+	project = models.ForeignKey(ProjectDetail,on_delete=models.CASCADE)
+	name = models.CharField(max_length=500)
+	description = models.TextField()
