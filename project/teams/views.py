8b09e7240c3b99403b99c2f3ed33417a64f08f6b
gjergjk71 <gjergjk71@gmail.com>
2018-08-03 15:52:28 +0200

    Craeted createTeam view

--- project/teams/views.py
+++ project/teams/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .forms import TeamForm
+from project.models import Project
 # Create your views here.
+
+def createTeam(request):
+	if request.method == "POST":
+		form = TeamForm(request.POST)
+		if form.is_valid():
+			team = form.save(commit=False)
+			projectID = request.POST.get("projectID")
+			team.project = Project.objects.get(pk=projectID)
+			team.save()
+	else:
+		lastPage = request.POST.get("lastPage")
+		return redirect(lastPage)
