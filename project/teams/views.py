1f5ace93e47c9b79f2d44cd438ebf877085942ff
gjergjk71 <gjergjk71@gmail.com>
2018-08-03 19:56:57 +0200

    showTeams view passes a dict of team:members to context

--- project/teams/views.py
+++ project/teams/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render,get_object_or_404
 from .forms import TeamForm
-from .models import Team
+from .models import Team,Member
 from project.models import ProjectDetail
 # Create your views here.
 
 def showTeams(request,project_id):
 	project = get_object_or_404(ProjectDetail,pk=project_id)
 	teamsList = Team.objects.all()
-	context = {"project":project,"teamsList":teamsList}
+	teamMembers = {}
+	for team in teamsList:
+		teamMembers[team] = len(Member.objects.filter(team=team))
+	context = {"project":project,"teamMembers":teamMembers}
 	return render(request,"teams/teamsList.html",context)
 
 def createTeam(request):
