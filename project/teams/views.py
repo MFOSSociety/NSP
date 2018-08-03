df6312f0e3e3da94dbe07c02b2ac381d5e5d7c30
gjergjk71 <gjergjk71@gmail.com>
2018-08-03 19:42:00 +0200

    Created showTeams view

--- project/teams/views.py
+++ project/teams/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .forms import TeamForm
+from .models import Team
 from project.models import Project
 # Create your views here.
+
+def showTeams(request,project_id):
+	project = get_object_or_404(Project,pk=project_id)
+	teamsList = Team.objects.all()
+	context = {"project":project,"teamsList":teamsList}
+	return render(request,"teams/teamsList.html",request)
 
 def createTeam(request):
 	if request.method == "POST":
@@ -9,7 +16,7 @@ def createTeam(request):
 		if form.is_valid():
 			team = form.save(commit=False)
 			projectID = request.POST.get("projectID")
-			team.project = Project.objects.get(pk=projectID)
+			team.project = get_object_or_404(Project,pk=projectID)
 			team.save()
 	else:
 		lastPage = request.POST.get("lastPage")
