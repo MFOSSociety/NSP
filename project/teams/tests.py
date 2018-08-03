45e9378a358a879b43826f6bf347d2c16ec6e8a9
gjergjk71 <gjergjk71@gmail.com>
2018-08-04 00:41:31 +0200

    Created test for LoginRequired decorator

--- project/teams/tests.py
+++ project/teams/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from project.models import ProjectDetail
+from .models import Team
+from django.contrib.auth.models import User
+import nsp.tests as tests
 
 # Create your tests here.
+
+class TestTeamsViewsLoginRequired(tests.TestLoginRequired):
+	def setUp(self):
+		self.user_object = User.objects.create(username="testing",
+												password="testing")
+		if tests.testDebug:
+			print("user_object created")
+		self.project_object = ProjectDetail.objects.create(project_name="testing",
+															initiated_by=self.user_object,
+															mentor_name="testing",
+															branch="testing",
+															description="testing")
+		if tests.testDebug:
+			print("project_object created")
+		self.team_object = Team.objects.create(project=self.project_object,
+											   description="testing")
+		if tests.testDebug:
+			print("team_object created")
+		self.pathnames_args = {"createTeam":[self.project_object.id],
+							   "deleteTeam":[self.team_object.id],
+							   "showTeams":[self.project_object.id]
+																}
