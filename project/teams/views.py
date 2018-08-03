e5e124f5d5792d51850ee8b317ff442cad212a2c
gjergjk71 <gjergjk71@gmail.com>
2018-08-03 19:47:32 +0200

    Renamed Project to ProjectDetail

--- project/teams/views.py
+++ project/teams/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render,get_object_or_404
 from .forms import TeamForm
 from .models import Team
-from project.models import Project
+from project.models import ProjectDetail
 # Create your views here.
 
 def showTeams(request,project_id):
-	project = get_object_or_404(Project,pk=project_id)
+	project = get_object_or_404(ProjectDetail,pk=project_id)
 	teamsList = Team.objects.all()
 	context = {"project":project,"teamsList":teamsList}
-	return render(request,"teams/teamsList.html",request)
+	return render(request,"teams/teamsList.html",context)
 
 def createTeam(request):
 	if request.method == "POST":
@@ -16,7 +16,7 @@ def createTeam(request):
 		if form.is_valid():
 			team = form.save(commit=False)
 			projectID = request.POST.get("projectID")
-			team.project = get_object_or_404(Project,pk=projectID)
+			team.project = get_object_or_404(ProjectDetail,pk=projectID)
 			team.save()
 	else:
 		lastPage = request.POST.get("lastPage")
