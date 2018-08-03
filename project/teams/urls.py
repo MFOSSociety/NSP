bc3e18404b0949b4161b6e269f800caff836142c
gjergjk71 <gjergjk71@gmail.com>
2018-08-04 00:07:07 +0200

    Added <project_id> to createTeam path

--- project/teams/urls.py
+++ project/teams/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("createTeam",views.createTeam,name="createTeam"),
+	path("createTeam/<project_id>",views.createTeam,name="createTeam"),
 	path("teams/<project_id>",views.showTeams,name="showTeams")
 ]
