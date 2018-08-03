0c2f28011c7bb4c15c2ce71d0d80bec8d5d8289e
gjergjk71 <gjergjk71@gmail.com>
2018-08-03 19:43:33 +0200

    Created showTeams path

--- project/teams/urls.py
+++ project/teams/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("createTeam",views.createTeam,name="createTeam")
+	path("createTeam",views.createTeam,name="createTeam"),
+	path("teams",views.showTeams,name="showTeams")
 ]
