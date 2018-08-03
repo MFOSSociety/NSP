3a9415a657ede351c81f621d5d6faaf762376769
gjergjk71 <gjergjk71@gmail.com>
2018-08-03 13:53:02 +0200

    Created model Member

--- project/teams/models.py
+++ project/teams/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from project.models import ProjectDetail
+from accounts.models import UserProfile
 # Create your models here.
 
 class Team(models.Model):
 	project = models.ForeignKey(ProjectDetail,on_delete=models.CASCADE)
 	name = models.CharField(max_length=500)
 	description = models.TextField()
+
+class Member(models.Model):
+	team = models.ForeignKey(Team,on_delete=models.CASCADE)
+	profile = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+	def __str__(self):
+		return "{} - {} {}".format(self.team.name,
+								   self.profile.user.firstname,
+								   self.profile.user.lastname)
