0224b069c8dd677ab6383bda11ba17025fa90098
gjergjk71 <gjergjk71@gmail.com>
2018-08-03 13:56:41 +0200

    Added field created to Team and joined field to Member model

--- project/teams/models.py
+++ project/teams/models.py
@@ -7,11 +7,13 @@ class Team(models.Model):
 	project = models.ForeignKey(ProjectDetail,on_delete=models.CASCADE)
 	name = models.CharField(max_length=500)
 	description = models.TextField()
+	created = models.DateField(auto_now_add=True)
 
 class Member(models.Model):
 	team = models.ForeignKey(Team,on_delete=models.CASCADE)
 	profile = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
+	joined = models.DateField(auto_now_add=True)
 	def __str__(self):
 		return "{} - {} {}".format(self.team.name,
-								   self.profile.user.firstname,
-								   self.profile.user.lastname)
+								   self.profile.user.first_name,
+								   self.profile.user.last_name)
