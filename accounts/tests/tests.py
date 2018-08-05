b04d9132cba4298058a237b16a3048d7a6dd1212
gjergjk71 <gjergjk71@gmail.com>
2018-08-06 00:48:21 +0200

    Created TestViews TestCase

---
+++ accounts/tests/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase,Client
+from django.shortcuts import reverse
+from accounts.models import UserProfile
+from django.contrib.auth.models import User
+# Create your tests here.
+
+class TestViews(TestCase):
+	def setUp(self):
+		self.client = Client()
+		self.user_object = User.objects.create(username="testing",
+											   password="testing")
+		self.client.force_login(self.user_object)
+
+	def test_views_200(self):
+		pathnames = ["home","view_profile","change_profile_picture",
+					"registersucess","signup","view_people",
+					"password_reset_complete","password_reset_done",
+					"reset_password","change_password","edit_profile"]
+		for pathname in pathnames:
+			url = reverse(pathname)
+			response = self.client.get(url)
+			self.assertEqual(response.status_code,200)
