1c30db50db5dd391142a3389edeec105b573814c
gjergjk71 <gjergjk71@gmail.com>
2018-08-05 17:49:09 +0200

    Added pragma for each line

--- project/teams/apps.py
+++ project/teams/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # pragma: no cover
 
 
-class TeamsConfig(AppConfig):
-    name = 'teams'
+class TeamsConfig(AppConfig): # pragma: no cover
+    name = 'teams' # pragma: no cover
