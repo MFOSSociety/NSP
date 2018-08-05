d89125be043bbdc0081029fd098ab8c33c2c2967
gjergjk71 <gjergjk71@gmail.com>
2018-08-05 18:51:40 +0200

    Created editTeam path

--- project/teams/urls.py
+++ project/teams/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	path("createTeam/<project_id>",views.createTeam,name="createTeam"),
+	path("editTeam/<team_id>",views.editTeam,name="editTeam"),
 	path("deleteTeam/<team_id>",views.deleteTeam,name="deleteTeam"),
 	path("teams/<project_id>",views.showTeams,name="showTeams"),
 	path("team/<team_id>",views.showTeam,name="showTeam")
