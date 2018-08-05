365d65221c29f81e1fcdd9f34c39e1b813c475a4
gjergjk71 <gjergjk71@gmail.com>
2018-08-05 19:43:02 +0200

    TestMemberModel TestCase ( commented )

--- project/teams/tests.py
+++ project/teams/tests.py
@@ -5,6 +5,7 @@ from .models import Team
 from django.contrib.auth.models import User
 from nsp.tests import TestLoginRequired
 from . import views
+from accounts.models import UserProfile
 # Create your tests here.
 
 class TestTeamsViewsLoginRequired(TestLoginRequired):
@@ -159,3 +160,29 @@ class TestCreateEditTeamViews(TestCase):
 		self.assertEquals(response_valid.url, reverse("showTeam",args=[self.team_object.id]))
 		self.assertEquals(response_invalid.status_code, 404)
 		self.assertEquals(response_valid_redirected.status_code, 200)
+
+"""
+def TestMemberModel(TestCase):
+	def setUp(self):
+		self.user_object = User.objects.create_superuser(
+			'testing',
+			'testing@example.com',
+			'testing',
+		)
+		self.userProfile_object = UserProfile.objects.get(user=self.user_object)
+		self.project_object = ProjectDetail.objects.create(project_name="testing",
+															initiated_by=self.user_object,
+															mentor_name="testing",
+															branch="testing",
+															description="testing")
+		self.team_object = Team.objects.create(project=self.project_object,
+											   description="testing")
+		self.member_object = Member.objects.create(team=self.team_object,
+												   profile=self.userProfile_object)
+	def test_str(self):
+		returnedString = "{} - {} {}".format(self.member_object.team.name,
+								   self.member_object.profile.user.first_name,
+								   self.member_object.profile.user.last_name)
+		print(returnedString)
+		self.assertEquals(str(self.member_object),returnedString)
+"""
