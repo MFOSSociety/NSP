4008fac394e10f65b65e5d590dd11f3bf24568d3
gjergjk71 <gjergjk71@gmail.com>
2018-08-06 11:49:22 +0200

    Testing status code 200 for all views

--- project/issueSolution/tests.py
+++ project/issueSolution/tests.py
@@ -6,6 +6,7 @@ from project.issueSolution.models import Issue,IssueComment,Solution,SolutionCom
 from django.contrib.auth.models import User
 
 # Create your tests here.
+tests.testDebug = False
 
 class TestIssueSolutionViewsLoginRequired(tests.TestLoginRequired):
 	def setUp(self):
@@ -58,3 +59,54 @@ class TestIssueSolutionViewsLoginRequired(tests.TestLoginRequired):
 								"editIssueSolution":editIssueSolution_args,"createIssueSolution":createIssueSolution_args,
 								"changeStatusIssueSolution":[self.project_object.id,"issue",self.issue_object.id,"open"],
 								"viewIssueSolution":viewIssueSolution_args}
+
+class TestViews(TestCase):
+	def setUp(self):
+		self.user_object = User.objects.create(username="testing",
+												password="testing")
+		self.user_object2 = User.objects.create(username="testing2",
+												password="testing2")
+		self.project_object = ProjectDetail.objects.create(project_name="testing",
+															initiated_by=self.user_object,
+															mentor_name="testing",
+															branch="testing",
+															description="testing")
+		self.issue_object = Issue.objects.create(user=self.user_object,
+												project=self.project_object,
+												title="testing",
+												description="testing")
+		self.solution_object = Solution.objects.create(issue=self.issue_object,
+														user=self.user_object,
+														title="testing",
+														description="testing")
+		self.issueComment_object = IssueComment.objects.create(issue=self.issue_object,
+																user=self.user_object,
+																comment="testing")
+
+		self.solutionComment_object = SolutionComment.objects.create(solution=self.solution_object,
+														user=self.user_object,
+														comment="testing")
+		self.client.force_login(self.user_object)
+	def test_views_200(self):
+		pathnames_args = {
+
+			"projectIssues":[[self.project_object.id,"open"],
+							[self.project_object.id,"closed"],
+							[self.project_object.id,"all"]],
+			"projectSolutions":[[self.project_object.id,"open"],
+							[self.project_object.id,"all"],
+							[self.project_object.id,"accepted"],
+							[self.project_object.id,"notaccepted"]],
+			"editIssueSolution":[[self.project_object.id,"issue",self.issue_object.id],
+								[self.project_object.id,"solution",self.solution_object.id]],
+			"createIssueSolution":[[self.project_object.id,"issue"],
+									[self.project_object.id,"solution"]],
+			"viewIssueSolution":[[self.project_object.id,"issue",self.issue_object.id],
+								[self.project_object.id,"solution",self.solution_object.id]]
+		}
+		for pathname,argsList in pathnames_args.items():
+			for args in argsList:
+				url = reverse(pathname,args=args)
+				response = self.client.get(url)
+				print(response)
+				self.assertEqual(response.status_code,200)
