23f6a723c1754a6b9b6ddbf1c41fdbe572ab8186
gjergjk71 <gjergjk71@gmail.com>
2018-08-09 15:51:15 +0200

    Created test for chat view

--- nspmessage/tests.py
+++ nspmessage/tests.py
@@ -15,3 +15,8 @@ class TestNspmessageViewsLoginRequired(tests.TestLoginRequired):
 		self.pathnames_args = {"chat":[],
 							  "chat_friend":[self.user_object2.id],
 							  "new_message":[self.user_object2.id]}
+	def test_chat_view(self):
+		self.client.force_login(self.user_object2)
+		url = reverse("chat")
+		response = self.client.get(url)
+		self.assertEqual(response.status_code,200)
