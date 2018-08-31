14e9057d8ba3786ef71b551fbc4c997844f1d49f
gjergjk71 <gjergjk71@gmail.com>
2018-09-01 01:49:20 +0200

    Configs for email back end

--- nsp/settings.py
+++ nsp/settings.py
@@ -167,7 +167,11 @@ LOGIN_REDIRECT_URL = '/account/'
 LOGIN_URL = '/account/login/'
 # For mailing purposes
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '<example@domain.com>'
+EMAIL_HOST_PASSWORD = '<password>'
+EMAIL_PORT = 587
 
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
