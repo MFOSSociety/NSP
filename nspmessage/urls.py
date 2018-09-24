98276900ef1ded802b34ebcf4418ac7c2a79a84f
gjergjk71 <gjergjk71@gmail.com>
2018-09-24 18:47:54 +0200

    Created send_message_api path

--- nspmessage/urls.py
+++ nspmessage/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.chat, name="chat"),
     path("<username>", views.chat_friend, name="chat_friend"),
-    path("<username>/new_message", views.new_message, name="new_message"),
-    path("get_messages_api/<receiver>",views.get_messages_api,name="get_messages_api")
+    path("get_messages_api/<receiver>",views.get_messages_api,name="get_messages_api"),
+    path("send_message_api/<receiver>",views.send_message_api,name="send_message_api"),
 ]
