bf1a7d5ce221590ed637d5fb25a2aa39eb034847
gjergjk71 <gjergjk71@gmail.com>
2018-09-24 16:12:56 +0200

    Chat updates every 0.5 seconds if new messages sent

--- nspmessage/static/nspmessage/chatFriend.js
+++ nspmessage/static/nspmessage/chatFriend.js
@@ -1,36 +1,71 @@
 $(".messages").animate({ scrollTop: $(document).height() }, "fast");
-/*
-$("#profile-img").click(function() {
-	$("#status-options").toggleClass("active");
-});
-/*
-$(".expand-button").click(function() {
-  $("#profile").toggleClass("expanded");
-	$("#contacts").toggleClass("expanded");
-});
+function getCookie(name) {
+	    var cookieValue = null;
+	    if (document.cookie && document.cookie !== '') {
+	        var cookies = document.cookie.split(';');
+	        for (var i = 0; i < cookies.length; i++) {
+	            var cookie = jQuery.trim(cookies[i]);
+	            // Does this cookie string begin with the name we want?
+	            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+	                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+	                break;
+	            }
+	        }
+	    }
+	    return cookieValue;
+	}
 
-$("#status-options ul li").click(function() {#
-	$("#profile-img").removeClass();
-	$("#status-online").removeClass("active");
-	$("#status-away").removeClass("active");
-	$("#status-busy").removeClass("active");
-	$("#status-offline").removeClass("active");
-	$(this).addClass("active");
-	
-	if($("#status-online").hasClass("active")) {
-		$("#profile-img").addClass("online");
-	} else if ($("#status-away").hasClass("active")) {
-		$("#profile-img").addClass("away");
-	} else if ($("#status-busy").hasClass("active")) {
-		$("#profile-img").addClass("busy");
-	} else if ($("#status-offline").hasClass("active")) {
-		$("#profile-img").addClass("offline");
-	} else {
-		$("#profile-img").removeClass();
-	};
-	
-	$("#status-options").removeClass("active");
-});
-*/
+var sender = document.getElementById("sender_username").value;
+var sender_id = document.getElementById("sender_id").value;
+var sender_user_profile_photo_url = document.getElementById("sender_user_profile_photo_url").value; 
+var receiver_user_profile_photo_url = document.getElementById("receiver_user_profile_photo_url").value; 
+var receiver = document.getElementById("receiver_username").value;
+var receiver_id = document.getElementById("receiver_id").value;
+var messages_len = 0;
+var messages_div = document.getElementById("messages");
 
+function clear() {
+	var messages_div = document.getElementById("messages");
+	messages_div.innerHTML = "";
+}
+function show(json) {
+	var ul = document.createElement("ul")
+	for (var key in json) {
+		if (json[key]["fields"].sender == sender_id) {
+			console.log(1);
+			li = document.createElement("li");
+			li.setAttribute("class","sent");
+			img = document.createElement("img");
+			img.src = sender_user_profile_photo_url;
+		} else {
+			li = document.createElement("li");
+			li.setAttribute("class","replies");
+			img = document.createElement("img");
+			img.src = receiver_user_profile_photo_url;		
+		}
+		p = document.createElement("p")
+		p.innerHTML = json[key]["fields"]["msg_content"];
+		li.appendChild(img);
+		li.appendChild(p);
+		ul.appendChild(li);
+	}
+	messages_div.appendChild(ul);
+}
 
+function update() {
+	var csrftoken = getCookie('csrftoken');
+	$.ajax({
+		method: "POST",
+		data: {"csrfmiddlewaretoken":csrftoken},
+		url: `/account/chat/get_messages_api/${receiver}`,
+	}).done(function (json){
+		console.log(json);
+		if (messages_len < json.length){
+			clear();
+			show(json);
+		}
+		messages_len = json.length
+	})
+window.setTimeout(update,500);
+}
+update();
